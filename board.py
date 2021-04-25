9710c6f19d89df5fd31462ae8158ff30b9ec3865
Shykial <szymon.jeziorski@interia.pl>
2021-04-25 23:49:05 +0200

    changed board to handle MxN dimensions, argparser now handles args for rows-num and columns-num

--- board.py
+++ board.py
@@ -1,22 +1,25 @@
 import random
-import re
 from typing import Union, Iterator
 
 
 class Board:
-    default_size: int = 4
+    default_rows_num: int = 4
+    default_columns_num: int = 4
 
-    def __init__(self, board_size: int = default_size):
-        self._board_size = board_size
-        tiles_values = list(range(board_size ** 2))
+    def __init__(self, rows_num: int = default_rows_num, columns_num: int = default_columns_num):
+        self._rows_num = rows_num
+        self._columns_num = columns_num
+
+        tiles_values = list(range(rows_num * columns_num))
         random.shuffle(tiles_values)
-        self._tiles = {index: value for index, value in zip(range(1, board_size ** 2 + 1), tiles_values)}
+
+        self._tiles = {index: value for index, value in zip(range(1, rows_num * columns_num + 1), tiles_values)}
         self._blank_title_pos = next(key for key, value in self._tiles.items() if value == 0)
 
     def __str__(self) -> str:
-        board_str_sequence = (f'{val:3d}\n\n' if not index % self._board_size else f'{val:3d}  '
+        board_str_sequence = (f'{val:3d}\n\n' if not index % self._columns_num else f'{val:3d}  '
                               for index, val in self._tiles.items())
-        return re.sub(r'(?<!\d)0', ' ', ''.join(board_str_sequence))
+        return ''.join(board_str_sequence).replace(' 0', '  ')
 
     def swap_titles(self, blank_title_pos: int, title_pos: int):
         self._blank_title_pos = title_pos
@@ -41,15 +44,14 @@ class Board:
             if key not in 'awsd':
                 continue
 
-            if key == 'a' and (blank_title_key := self._blank_title_pos) % self._board_size:
+            if key == 'a' and (blank_title_key := self._blank_title_pos) % self._columns_num:
                 self.swap_titles(blank_title_key, blank_title_key + 1)
 
-            elif key == 'd' and (blank_title_key := self._blank_title_pos) % self._board_size != 1:
+            elif key == 'd' and (blank_title_key := self._blank_title_pos) % self._columns_num != 1:
                 self.swap_titles(blank_title_key, blank_title_key - 1)
 
-            elif key == 's' and (blank_title_key := self._blank_title_pos) / self._board_size > 1:
-                self.swap_titles(blank_title_key, blank_title_key - self._board_size)
+            elif key == 's' and (blank_title_key := self._blank_title_pos) / self._columns_num > 1:
+                self.swap_titles(blank_title_key, blank_title_key - self._columns_num)
 
-            elif key == 'w' and \
-                    (blank_title_key := self._blank_title_pos) / self._board_size <= self._board_size - 1:
-                self.swap_titles(blank_title_key, blank_title_key + self._board_size)
+            elif key == 'w' and (blank_title_key := self._blank_title_pos) / self._columns_num <= self._rows_num - 1:
+                self.swap_titles(blank_title_key, blank_title_key + self._columns_num)
--- main.py
+++ main.py
@@ -3,27 +3,33 @@ import argparse
 from board import Board
 
 
-def validate_parsed_args(parsed_args: argparse.Namespace):
-    if parsed_args.board_size and parsed_args.board_size <= 1:
-        raise ValueError(f'Board size value must be no less than 2, provided: {parsed_args.board_size}')
-
-
 def get_parsed_args() -> argparse.Namespace:
     arg_parser = argparse.ArgumentParser()
 
-    arg_parser.add_argument('-s', '--board-size', metavar='NUM', type=int, help='Size of the board used in simulation')
+    arg_parser.add_argument('-r', '--rows-num', metavar='NUM', type=int, help='Number of rows on the board')
+    arg_parser.add_argument('-c', '--columns-num', metavar='NUM', type=int, help='Number of columns on the board')
 
     parsed_args = arg_parser.parse_args()
     validate_parsed_args(parsed_args)
     return parsed_args
 
 
+def validate_parsed_args(parsed_args: argparse.Namespace):
+    for arg_name, arg_value in {'rows_num': parsed_args.rows_num, 'columns_num': parsed_args.columns_num}.items():
+        if arg_value and arg_value < 2:
+            raise ValueError(f'{arg_name} value must be no less than 2, provided: {arg_value}')
+
+
 def main():
     args = get_parsed_args()
-    board = Board(args.board_size if args.board_size else 4)
-    print(board)
-    # board.play()
-    print(board._board_size)
+
+    # getting kwargs dict and filtering non-None values
+    board_kwargs = {name: value for name, value
+                    in {'columns_num': args.columns_num, 'rows_num': args.rows_num}.items() if value}
+
+    board = Board(**board_kwargs)
+
+    board.play()
 
 
 if __name__ == '__main__':
